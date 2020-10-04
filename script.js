8e59b548c8b6f322803796d0509206b784483a36
Chloe Wilson <chloewilson510@gmail.com>
2020-10-03 23:42:04 -0600

    added arrays of lowercase, uppercase, numbers and special characters

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Here I'm putting the Arrays of lowercase, uppercase, numerical and special characters
+var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericalArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialArray = ["!", "#", "$","%", "&"]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +15,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+console.log( password_generator() );
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
